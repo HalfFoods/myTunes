27a3b6a7d6fbc011f1f9c0194f92c16f84755ade
Jeffrey <Jeffrey.Chen.009@gmail.com>
2019-10-29 11:43:44 -0400

    Added start of main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "linkedlist.h"
+
+int main(){
+  printf("Linked List tests:\n");
+  printf("--------------------\n");
+  printf("Testing print_list:\n")
+  
+  return 0;
+}
