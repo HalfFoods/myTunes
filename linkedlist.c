6c3000e405490d067fcc6b468e021f4aa1462e20
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-10-29 11:52:19 -0400

    first function started and added include headers

--- linkedlist.c
+++ linkedlist.c
@@ -1,4 +1,7 @@
-//Jeffrey
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "linkedlist.h"
 struct song_node * insert_front(){
 
 }
@@ -13,15 +16,18 @@ struct song_node * get_node(){
 }
 
 //Vivian
-struct song_node * get_artist(){
+struct song_node * get_artist(struct song_node * playlist, char * artist){
+  struct song_node * current = playlist;
+  while (current->next || current->artist != artist){
+
+  }
+}
+struct song_node * get_random(struct song_node * playlist){
 
 }
-struct song_node * get_random(){
+struct song_node * remove_node(struct song_node * playlist, char * artist, char * song){
 
 }
-struct song_node * remove_node(){
-
-}
-struct song_node * free_list(){
+struct song_node * free_list(struct song_node * playlist){
 
 }
