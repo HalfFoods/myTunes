c427ae30cd461002578bdaee22420e788eb3824e
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-10-29 11:58:33 -0400

    get_artist function completed

--- linkedlist.c
+++ linkedlist.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 #include "linkedlist.h"
+
 struct song_node * insert_front(){
 
 }
 struct song_node * insert_order(){
 
 }
-void print_list(struct * song_node s){
-  while(s){
-    printf("")
-  }
+void print_list(){
+
 }
 struct song_node * get_node(){
 
@@ -20,10 +19,18 @@ struct song_node * get_node(){
 //Vivian
 struct song_node * get_artist(struct song_node * playlist, char * artist){
   struct song_node * current = playlist;
-  while (current->next || current->artist != artist){
-
+  //while next node exists and the current artist is not correct
+  while (current->next && strcmp(current->artist, artist) != 0){
+    current = current-> next;
   }
+  //If the artists match
+  if (strcmp(current->artist, artist) == 0){
+    return current;
+  }
+  //If artist is not in the list
+  return NULL;
 }
+
 struct song_node * get_random(struct song_node * playlist){
 
 }
