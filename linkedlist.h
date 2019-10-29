a2e4ef40d503abcdae00a59f978f30f4151c1d16
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-10-29 11:39:36 -0400

    headers added

---
+++ linkedlist.h
@@ -0,0 +1,15 @@
+struct song_node{
+  char name[100];
+  char artist[100];
+  struct song_node *next;
+};
+struct song_node * table[27];
+
+struct song_node * insert_front();
+struct song_node * insert_order();
+struct song_node * print_list();
+struct song_node * get_node();
+struct song_node * get_artist()
+struct song_node * get_random();
+struct song_node * remove_node();
+struct song_node * free_list();
