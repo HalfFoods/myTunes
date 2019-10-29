5eeb334478320f57f2ad393a82ef4942e661070d
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-10-29 11:41:15 -0400

    function outlines added

---
+++ linkedlist.c
@@ -0,0 +1,24 @@
+struct song_node * insert_front(){
+
+}
+struct song_node * insert_order(){
+
+}
+void print_list(){
+
+}
+struct song_node * get_node(){
+
+}
+struct song_node * get_artist(){
+
+}
+struct song_node * get_random(){
+
+}
+struct song_node * remove_node(){
+
+}
+struct song_node * free_list(){
+
+}
