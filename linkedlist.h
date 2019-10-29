db4049346e97f323bc75dfd880c0802b259ab487
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-10-29 11:40:46 -0400

    header fixed

--- linkedlist.h
+++ linkedlist.h
@@ -5,9 +5,9 @@ struct song_node{
 };
 struct song_node * table[27];
 
+void print_list();
 struct song_node * insert_front();
 struct song_node * insert_order();
-struct song_node * print_list();
 struct song_node * get_node();
 struct song_node * get_artist()
 struct song_node * get_random();
