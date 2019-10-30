230caefe027dfb6c114405a07ce76ddd8a2f6ede
Jeffrey <Jeffrey.Chen.009@gmail.com>
2019-10-30 11:19:41 -0400

    Completed insert_front and print_list

--- linkedlist.h
+++ linkedlist.h
@@ -1,15 +1,18 @@
 struct song_node{
-  char name[100];
   char artist[100];
+  char name[100];
   struct song_node *next;
 };
 struct song_node * table[27];
 
-void print_list(struct * song_node s);
-struct song_node * insert_front();
+struct song_node * insert_front(struct song_node * s, char artist [100], char name [100]);
+void print_list(struct song_node * s);
+
+/*
 struct song_node * insert_order();
 struct song_node * get_node();
-struct song_node * get_artist()
+struct song_node * get_artist();
 struct song_node * get_random();
 struct song_node * remove_node();
 struct song_node * free_list();
+*/
