d30101d5b2c4470357fd0c09bffaf345cd5494c6
Jeffrey <Jeffrey.Chen.009@gmail.com>
2019-11-02 21:11:37 -0400

    Completed writing and testing insert_order

--- linkedlist.c
+++ linkedlist.c
@@ -22,26 +22,34 @@ void print_list(struct song_node * s){
 
 struct song_node * insert_order(struct song_node * s, char artist [100], char name [100]){
   struct song_node * cur = s;
+  // Empty list
+  if (!cur){
+    return insert_front(cur, artist, name);
+  }
+  // Insert at front
+  if (strcmp(artist, cur->artist) < 0){
+    return insert_front(cur, artist, name);
+  }
+  // Find place in list
   while(cur->next){
     if (strcmp(artist, cur->next->artist) < 0){
       cur->next = insert_front(cur->next, artist, name);
       return s;
     }
-    /*
-    if (strcmp(artist, cur->artist) == 0){
-      while(cur && (strcmp(artist, cur->artist) == 0)){
-        if(strcmp(name, cur->name) < 0){
-          cur = insert_front(cur, artist, name);
-          return s;
-        }
-        cur = cur->next;
+    // Same artist
+    if (strcmp(artist, cur->next->artist) == 0){
+      if(strcmp(name, cur->next->name) < 0){
+        cur->next = insert_front(cur->next, artist, name);
+        return s;
       }
-      cur = insert_front(cur,artist, name);
-      return s;
     }
-    */
-    cur = cur-> next;
+    cur = cur->next;
   }
+  struct song_node * back = malloc(sizeof(struct song_node));
+  strcpy(back->artist, artist);
+  strcpy(back->name, name);
+  cur->next = back;
+  return s;
 }
 /*
 //Jeffrey
